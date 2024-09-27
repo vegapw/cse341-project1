8d5625b0a160f64887eb8e0faaad8738958b73a8
Fredy Vega <fredyvega@Fredys-MacBook-Air.local>
2024-09-27 10:54:04 -0500

    Project 1 : Part 2, Header updated in app.js to allow access control

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const mongoDB = require('./data/database');
 const port = process.env.PORT || 3000;
 
 app .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        next();
+    })
     .use('/', routes);
 
 
